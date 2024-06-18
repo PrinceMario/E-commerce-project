b91f13a81085732da42b94a1ccd5016eead42a89
Prince Mario <princemaria1990@gmail.com>
2024-06-19 00:29:57 +0530

    Add files via upload

---
+++ Whole dataset details.sql
@@ -0,0 +1,25 @@
+-- List Tables on Database
+show tables;
+
+-- order_details
+select * from order_details
+limit 5; 
+desc order_details;
+
+-- orders
+select * from orders
+limit 5; 
+desc orders;
+
+-- products
+select * from products 
+limit 5; 
+desc products;
+
+-- users
+select * from users 
+limit 5; 
+desc users;
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
